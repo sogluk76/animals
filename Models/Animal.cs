c8d2f95c4b6d8e04adaab78411a0a7f7eb5b9093
John Doe <johndoe@example.com>
2023-03-08 12:06:29 +0100

    Ajoutez des fichiers projet.

---
+++ Models/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public abstract class Animal
+{
+    public string Name { get; private set; }
+
+    public Animal()
+    {
+        Name = "Lukas";
+    }
+
+    public Animal(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Move();
+
+    public void Sleep()
+    {
+        Console.WriteLine($"{Name} est endormi.");
+    }
+}
+
+public class Dragon : Animal
+{
+    public Dragon() : base()
+    {
+    }
+
+    public Dragon(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} vole dans les airs.");
+    }
+}
+
+public class Griffin : Animal
+{
+    public Griffin() : base()
+    {
+    }
+
+    public Griffin(string name) : base(name)
+    {
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine($"{Name} s'envole .");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Animal myAnimal = new Animal("Roucoups");
+        //myAnimal.Move();
+        //myAnimal.Sleep();
+
+        //Animal anotherAnimal = new Animal();
+        //anotherAnimal.Name = "Groudon";
+        //anotherAnimal.Move();
+        //anotherAnimal.Sleep();
+
+        Dragon myDragon = new Dragon("Dragon");
+        myDragon.Move();
+        myDragon.Sleep();
+
+        Griffin myGriffin = new Griffin("Griffin");
+        myGriffin.Move();
+        myGriffin.Sleep();
+    }
+}
